81e09b486f9feb79fa0815f977bb80cf1c20d37f
Itraa1 <160110364+Itraa1@users.noreply.github.com>
2026-06-09 11:55:39 +0300

    changed schema

--- src/api/course/controllers/course.ts
+++ src/api/course/controllers/course.ts
@@ -4,4 +4,18 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::course.course');
+export default factories.createCoreController('api::course.course',
+    ({ strapi }) => ({
+        async create(ctx) {
+            
+            const userId = ctx.state.user.id;
+
+            ctx.request.body.data = {
+                ...ctx.request.body.data,
+                users_permissions_user: userId,
+            };
+            const response = await super.create(ctx);
+
+            return response;
+        },
+    }));
--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,21 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface TopicTopic extends Struct.ComponentSchema {
+  collectionName: 'components_topic_topics';
+  info: {
+    displayName: 'topic';
+    icon: 'book';
+  };
+  attributes: {
+    content: Schema.Attribute.RichText;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'topic.topic': TopicTopic;
+    }
+  }
+}
